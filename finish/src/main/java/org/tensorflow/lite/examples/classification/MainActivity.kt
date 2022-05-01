e4cb8205bea1a960105212f52e6c705c13ae93d0
evgeniy sarzhan <ysar@solvve.com>
2022-05-01 12:25:23 +0300

    run

--- finish/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
+++ finish/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
@@ -17,43 +17,39 @@
 package org.tensorflow.lite.examples.classification
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Matrix
+import android.graphics.*
 import android.os.Bundle
 import android.util.Log
-import android.util.Size
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
-import org.tensorflow.lite.examples.classification.ml.FlowerModel
-import org.tensorflow.lite.examples.classification.ui.RecognitionAdapter
-import org.tensorflow.lite.examples.classification.util.YuvToRgbConverter
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.tensorflow.lite.examples.classification.util.CameraProcess
+import org.tensorflow.lite.examples.classification.util.ImageProcess
+import org.tensorflow.lite.examples.classification.util.Yolov5TFLiteDetector
 import org.tensorflow.lite.examples.classification.viewmodel.Recognition
 import org.tensorflow.lite.examples.classification.viewmodel.RecognitionListViewModel
-import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.support.model.Model
 import java.util.concurrent.Executors
-import org.tensorflow.lite.gpu.CompatibilityList
 
 // Constants
 private const val MAX_RESULT_DISPLAY = 3 // Maximum number of results displayed
 private const val TAG = "TFL Classify" // Name for logging
 private const val REQUEST_CODE_PERMISSIONS = 999 // Return code after asking for permission
 private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA) // permission needed
+private var yolov5TFLiteDetector: Yolov5TFLiteDetector? = null
 
 // Listener for the result of the ImageAnalyzer
 typealias RecognitionListener = (recognition: List<Recognition>) -> Unit
@@ -76,40 +72,70 @@ class MainActivity : AppCompatActivity() {
     private val viewFinder by lazy {
         findViewById<PreviewView>(R.id.viewFinder) // Display the preview image from Camera
     }
+    private val boxLabelCanvas by lazy {findViewById<ImageView>(R.id.box_label_canvas)}
 
     // Contains the recognition result. Since  it is a viewModel, it will survive screen rotations
     private val recogViewModel: RecognitionListViewModel by viewModels()
+
+    var rotation = 0
+
+    private val cameraProcess: CameraProcess = CameraProcess()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewFinder.scaleType = PreviewView.ScaleType.FILL_START
+        // 获取手机摄像头拍照旋转参数
+
+        // 获取手机摄像头拍照旋转参数
+        rotation = windowManager.defaultDisplay.rotation
+        Log.i("image", "rotation: $rotation")
+
+        // 初始化加载yolov5s
+        initModel("best")
+
         // Request camera permissions
         if (allPermissionsGranted()) {
-            startCamera()
+//            startCamera()
+            cameraProcess.startCamera(this@MainActivity, ImageAnalyzer(this, viewFinder,rotation, yolov5TFLiteDetector!!, boxLabelCanvas), viewFinder)
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
             )
         }
 
-        // Initialising the resultRecyclerView and its linked viewAdaptor
-        val viewAdapter = RecognitionAdapter(this)
-        resultRecyclerView.adapter = viewAdapter
+//        // Initialising the resultRecyclerView and its linked viewAdaptor
+//        val viewAdapter = RecognitionAdapter(this)
+//        resultRecyclerView.adapter = viewAdapter
+//
+//        // Disable recycler view animation to reduce flickering, otherwise items can move, fade in
+//        // and out as the list change
+//        resultRecyclerView.itemAnimator = null
+//
+//        // Attach an observer on the LiveData field of recognitionList
+//        // This will notify the recycler view to update every time when a new list is set on the
+//        // LiveData field of recognitionList.
+//        recogViewModel.recognitionList.observe(this,
+//            Observer {
+//                viewAdapter.submitList(it)
+//            }
+//        )
 
-        // Disable recycler view animation to reduce flickering, otherwise items can move, fade in
-        // and out as the list change
-        resultRecyclerView.itemAnimator = null
+    }
 
-        // Attach an observer on the LiveData field of recognitionList
-        // This will notify the recycler view to update every time when a new list is set on the
-        // LiveData field of recognitionList.
-        recogViewModel.recognitionList.observe(this,
-            Observer {
-                viewAdapter.submitList(it)
-            }
-        )
-
+    private fun initModel(modelName: String) {
+        // 加载模型
+        try {
+            yolov5TFLiteDetector = Yolov5TFLiteDetector()
+            yolov5TFLiteDetector!!.setModelFile(modelName)
+            //            this.yolov5TFLiteDetector.addNNApiDelegate();
+            yolov5TFLiteDetector!!.addGPUDelegate()
+            yolov5TFLiteDetector!!.initialModel(this)
+            Log.i("model", "Success loading model" + yolov5TFLiteDetector!!.getModelFile())
+        } catch (e: java.lang.Exception) {
+            Log.e("image", "load model error: " + e.message + e.toString())
+        }
     }
 
     /**
@@ -131,7 +157,8 @@ class MainActivity : AppCompatActivity() {
     ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera()
+//                startCamera()
+                cameraProcess.startCamera(this@MainActivity, ImageAnalyzer(this, viewFinder,rotation, yolov5TFLiteDetector!!, boxLabelCanvas), viewFinder)
             } else {
                 // Exit the app if permission is not granted
                 // Best practice is to explain and offer a chance to re-request but this is out of
@@ -163,23 +190,21 @@ class MainActivity : AppCompatActivity() {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
                 .build()
 
             imageAnalyzer = ImageAnalysis.Builder()
                 // This sets the ideal size for the image to be analyse, CameraX will choose the
                 // the most suitable resolution which may not be exactly the same or hold the same
                 // aspect ratio
-                .setTargetResolution(Size(224, 224))
+//                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
                 // How the Image Analyser should pipe in input, 1. every frame but drop no frame, or
                 // 2. go to the latest frame and may drop some frame. The default is 2.
                 // STRATEGY_KEEP_ONLY_LATEST. The following line is optional, kept here for clarity
                 .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build()
                 .also { analysisUseCase: ImageAnalysis ->
-                    analysisUseCase.setAnalyzer(cameraExecutor, ImageAnalyzer(this) { items ->
-                        // updating the list of recognised objects
-                        recogViewModel.updateData(items)
-                    })
+                    analysisUseCase.setAnalyzer(cameraExecutor, ImageAnalyzer(this, viewFinder,rotation, yolov5TFLiteDetector!!, boxLabelCanvas))
                 }
 
             // Select camera, back is the default. If it is not available, choose front camera
@@ -206,98 +231,113 @@ class MainActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
-    private class ImageAnalyzer(ctx: Context, private val listener: RecognitionListener) :
+
+    private class ImageAnalyzer(
+        val ctx: Context, val previewView: PreviewView, val rotation: Int,
+        val yolov5TFLiteDetector: Yolov5TFLiteDetector, val boxLabelCanvas: ImageView
+    ) :
         ImageAnalysis.Analyzer {
+        val imageProcess: ImageProcess = ImageProcess()
 
-        // TODO 1: Add class variable TensorFlow Lite Model
-        // Initializing the flowerModel by lazy so that it runs in the same thread when the process
-        // method is called.
-        private val flowerModel: FlowerModel by lazy{
+        class Result( var bitmap: Bitmap)
 
-            // TODO 6. Optional GPU acceleration
-            val compatList = CompatibilityList()
+        override fun analyze(image: ImageProxy) {
 
-            val options = if(compatList.isDelegateSupportedOnThisDevice) {
-                Log.d(TAG, "This device is GPU Compatible ")
-                Model.Options.Builder().setDevice(Model.Device.GPU).build()
-            } else {
-                Log.d(TAG, "This device is GPU Incompatible ")
-                Model.Options.Builder().setNumThreads(4).build()
-            }
+            val previewHeight = previewView.height
+            val previewWidth = previewView.width
 
-            // Initialize the Flower Model
-            FlowerModel.newInstance(ctx, options)
-        }
+            // Здесь Observable помещает логику анализа изображений в подпоток и получает соответствующие
+            // данные обратно при рендеринге пользовательского интерфейса, чтобы избежать отставания внешнего пользовательского интерфейса.
+            Observable.create(ObservableOnSubscribe<Result?> { emitter ->
+                Log.i("image", "previewWidth $previewWidth/$previewHeight")
+                val yuvBytes = arrayOfNulls<ByteArray>(3)
+                val planes = image.planes
+                val imageHeight = image.height
+                val imagewWidth = image.width
+                Log.i("image", "imagewWidth$imagewWidth/$imageHeight")
+                imageProcess.fillBytes(planes, yuvBytes)
+                val yRowStride = planes[0].rowStride
+                val uvRowStride = planes[1].rowStride
+                val uvPixelStride = planes[1].pixelStride
+                val rgbBytes = IntArray(imageHeight * imagewWidth)
+                imageProcess.YUV420ToARGB8888(
+                    yuvBytes[0],
+                    yuvBytes[1],
+                    yuvBytes[2],
+                    imagewWidth,
+                    imageHeight,
+                    yRowStride,
+                    uvRowStride,
+                    uvPixelStride,
+                    rgbBytes)
 
-        override fun analyze(imageProxy: ImageProxy) {
+                // Исходное изображение
+                val imageBitmap = Bitmap.createBitmap(imagewWidth, imageHeight, Bitmap.Config.ARGB_8888)
+                imageBitmap.setPixels(rgbBytes, 0, imagewWidth, 0, 0, imagewWidth, imageHeight)
 
-            val items = mutableListOf<Recognition>()
-
-            // TODO 2: Convert Image to Bitmap then to TensorImage
-            val tfImage = TensorImage.fromBitmap(toBitmap(imageProxy))
-
-            // TODO 3: Process the image using the trained model, sort and pick out the top results
-            val outputs = flowerModel.process(tfImage)
-                .probabilityAsCategoryList.apply {
-                    sortByDescending { it.score } // Sort with highest confidence first
-                }.take(MAX_RESULT_DISPLAY) // take the top results
-
-            // TODO 4: Converting the top probability items into a list of recognitions
-            for (output in outputs) {
-                items.add(Recognition(output.label, output.score))
-            }
-
-//            // START - Placeholder code at the start of the codelab. Comment this block of code out.
-//            for (i in 0 until MAX_RESULT_DISPLAY){
-//                items.add(Recognition("Fake label $i", Random.nextFloat()))
-//            }
-//            // END - Placeholder code at the start of the codelab. Comment this block of code out.
-
-            // Return the result
-            listener(items.toList())
-
-            // Close the image,this tells CameraX to feed the next image to the analyzer
-            imageProxy.close()
-        }
-
-        /**
-         * Convert Image Proxy to Bitmap
-         */
-        private val yuvToRgbConverter = YuvToRgbConverter(ctx)
-        private lateinit var bitmapBuffer: Bitmap
-        private lateinit var rotationMatrix: Matrix
-
-        @SuppressLint("UnsafeExperimentalUsageError")
-        private fun toBitmap(imageProxy: ImageProxy): Bitmap? {
-
-            val image = imageProxy.image ?: return null
-
-            // Initialise Buffer
-            if (!::bitmapBuffer.isInitialized) {
-                // The image rotation and RGB image buffer are initialized only once
-                Log.d(TAG, "Initalise toBitmap()")
-                rotationMatrix = Matrix()
-                rotationMatrix.postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
-                bitmapBuffer = Bitmap.createBitmap(
-                    imageProxy.width, imageProxy.height, Bitmap.Config.ARGB_8888
+                // Изображение адаптировано к экрану fill_start формат bitmap
+                val scale = Math.max(
+                    previewHeight / (if (rotation % 180 == 0) imagewWidth else imageHeight).toDouble(),
+                    previewWidth / (if (rotation % 180 == 0) imageHeight else imagewWidth).toDouble()
                 )
-            }
+                val fullScreenTransform = imageProcess.getTransformationMatrix(
+                    imagewWidth, imageHeight,
+                    (scale * imageHeight).toInt(), (scale * imagewWidth).toInt(),
+                    if (rotation % 180 == 0) 90 else 0, false
+                )
 
-            // Pass image to an image analyser
-            yuvToRgbConverter.yuvToRgb(image, bitmapBuffer)
+                // Полноразмерное растровое изображение для предварительного просмотра
+                val fullImageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imagewWidth, imageHeight, fullScreenTransform, false)
+                // Обрезаем растровое изображение до того же размера, что и предварительный просмотр на экране
+                val cropImageBitmap = Bitmap.createBitmap(
+                    fullImageBitmap, 0, 0,
+                    previewWidth, previewHeight
+                )
 
-            // Create the Bitmap in the correct orientation
-            return Bitmap.createBitmap(
-                bitmapBuffer,
-                0,
-                0,
-                bitmapBuffer.width,
-                bitmapBuffer.height,
-                rotationMatrix,
-                false
-            )
+                // Растровое изображение входа модели
+                val previewToModelTransform = imageProcess.getTransformationMatrix(
+                    cropImageBitmap.width, cropImageBitmap.height,
+                    yolov5TFLiteDetector.inputSize.width,
+                    yolov5TFLiteDetector.inputSize.height,
+                    0, false)
+                val modelInputBitmap = Bitmap.createBitmap(cropImageBitmap, 0, 0,
+                    cropImageBitmap.width, cropImageBitmap.height,
+                    previewToModelTransform, false)
+                val modelToPreviewTransform = Matrix()
+                previewToModelTransform.invert(modelToPreviewTransform)
+                val recognitions: ArrayList<org.tensorflow.lite.examples.classification.util.Recognition>? = yolov5TFLiteDetector.detect(modelInputBitmap)
+                val emptyCropSizeBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888)
+                val cropCanvas = Canvas(emptyCropSizeBitmap)
+                // Пограничная кисть
+                val boxPaint = Paint()
+                boxPaint.strokeWidth = 5f
+                boxPaint.style = Paint.Style.STROKE
+                boxPaint.color = Color.RED
+                // Кисть шрифта
+                val textPain = Paint()
+                textPain.textSize = 50f
+                textPain.color = Color.RED
+                textPain.style = Paint.Style.FILL
+                for (res in recognitions!!) {
+                    val location: RectF = res.getLocation()
+                    val label: String = res.getLabelName()
+                    val confidence: Float = res.getConfidence()
+                    modelToPreviewTransform.mapRect(location)
+                    cropCanvas.drawRect(location, boxPaint)
+                    cropCanvas.drawText(label + ":" + String.format("%.2f", confidence), location.left, location.top, textPain)
+                }
+                image.close()
+                emitter.onNext(Result(emptyCropSizeBitmap))
+            }).subscribeOn(Schedulers.io()) // Определите здесь watchee, который является потоком в коде выше, если он не определен,
+                // то это главный поток синхронный, а не асинхронный
+                // Здесь мы возвращаемся в основной поток, где наблюдатель получает данные, отправленные эмиттером, и обрабатывает их
+                ?.observeOn(AndroidSchedulers.mainThread()) // Здесь мы возвращаемся в главный поток, чтобы обработать
+                // данные обратного вызова из дочернего потока.
+                ?.subscribe { result: Result? ->
+                    boxLabelCanvas.setImageBitmap(result?.bitmap)
+                }
+
+
         }
-
     }
-
 }
